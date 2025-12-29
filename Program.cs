028b55968776cdf59424bd0512df26ac5730fc4d
SJMcode <safir.jameel@gmail.com>
2025-12-29 05:44:36 +0100

    added 8080 for Railway

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.WebHost.ConfigureKestrel(options => { options.ListenAnyIP(8080); });
 
 // CORS for React frontend
 builder.Services.AddCors(options =>
