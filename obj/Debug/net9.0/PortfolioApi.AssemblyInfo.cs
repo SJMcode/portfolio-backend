df528d12302eca291376777fb975e1749b9b17b9
SJMcode <safir.jameel@gmail.com>
2025-12-29 05:50:01 +0100

    Fix .NET 9 fallback folder issue for Railway

--- obj/Debug/net9.0/PortfolioApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/PortfolioApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+214fa7c4d2959cc9bef74c199abd9c5efe2522ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+028b55968776cdf59424bd0512df26ac5730fc4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
