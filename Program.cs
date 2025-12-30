0627664162ac4e55c11fe71d666a18328b4e47b3
SJMcode <safir.jameel@gmail.com>
2025-12-30 05:50:49 +0100

    add smartek domain

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
-        policy.WithOrigins("http://localhost:5173", "https://sjmcode.github.io")
+        policy.WithOrigins("http://localhost:5173", "https://sjmcode.github.io", "https://smarteksolutions.se")
               .AllowAnyMethod()
               .AllowAnyHeader());
 });
